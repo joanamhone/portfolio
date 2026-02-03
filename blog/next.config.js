51ee8fc9f649c9aa4b37a9b9d88ad702a3f33e22
Joana P. Mhone <J.Mhone@tnm.co.mw>
2026-02-03 15:28:33 +0200

    blogs server

--- blog/next.config.js
+++ blog/next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
   },
   basePath: '/blog',
   assetPrefix: '/blog/',
+  distDir: '.next',
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
